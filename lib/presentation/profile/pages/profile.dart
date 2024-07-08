24f760839807f94f4bcd9d649e0077431111b5b6
Syed-Abdullah-G <syedabdullahcslab@gmail.com>
2024-07-08 17:30:38 +0530

    fixed appbar theme error in profile

--- lib/presentation/profile/pages/profile.dart
+++ lib/presentation/profile/pages/profile.dart
@@ -17,18 +17,21 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BasicAppbar(
-        backgroundColor: Color(0xff2C2B2B) ,
-        title: Text(
-          'Profile'
-        ),
-      ),
+      appBar: AppBar(
+          backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+          title: Center(
+              child: Padding(
+            padding: const EdgeInsets.only(right: 50),
+            child: Text('Profile'),
+          ))),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-           _profileInfo(context),
-           const SizedBox(height: 30,),
-           _favoriteSongs()
+          _profileInfo(context),
+          const SizedBox(
+            height: 30,
+          ),
+          _favoriteSongs()
         ],
       ),
     );
@@ -38,63 +41,54 @@ class ProfilePage extends StatelessWidget {
     return BlocProvider(
       create: (context) => ProfileInfoCubit()..getUser(),
       child: Container(
-        height: MediaQuery.of(context).size.height / 3.5 ,
+        height: MediaQuery.of(context).size.height / 3.5,
         width: double.infinity,
         decoration: BoxDecoration(
-          color: context.isDarkMode ? const Color(0xff2C2B2B) : Colors.white,
-          borderRadius: const BorderRadius.only(
-            bottomRight: Radius.circular(50),
-            bottomLeft: Radius.circular(50)
-          )
-        ),
-        child: BlocBuilder<ProfileInfoCubit,ProfileInfoState>(
-        builder: (context, state) {
-          if(state is ProfileInfoLoading) {
-            return Container(
-              alignment: Alignment.center,
-              child: const CircularProgressIndicator()
-            );
-          } 
-          if(state is ProfileInfoLoaded) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 90,
-                  width: 90,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        state.userEntity.imageURL!
-                      )
-                    )
+            color: context.isDarkMode ? const Color(0xff2C2B2B) : Colors.white,
+            borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(50),
+                bottomLeft: Radius.circular(50))),
+        child: BlocBuilder<ProfileInfoCubit, ProfileInfoState>(
+          builder: (context, state) {
+            if (state is ProfileInfoLoading) {
+              return Container(
+                  alignment: Alignment.center,
+                  child: const CircularProgressIndicator());
+            }
+            if (state is ProfileInfoLoaded) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 90,
+                    width: 90,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            image: NetworkImage(state.userEntity.imageURL!))),
                   ),
-                ),
-                const SizedBox(height: 15,),
-                Text(
-                  state.userEntity.email!
-                ),
-                const SizedBox(height: 10,),
-                Text(
-                  state.userEntity.fullName!,
-                  style: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold
+                  const SizedBox(
+                    height: 15,
                   ),
-                )
-              ],
-            );
-          }
+                  Text(state.userEntity.email!),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    state.userEntity.fullName!,
+                    style: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold),
+                  )
+                ],
+              );
+            }
 
-          if(state is ProfileInfoFailure) {
-            return const Text(
-              'Please try again'
-            );
-          }
-          return Container();
-        }, 
-      ),
+            if (state is ProfileInfoFailure) {
+              return const Text('Please try again');
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
@@ -103,109 +97,108 @@ class ProfilePage extends StatelessWidget {
     return BlocProvider(
       create: (context) => FavoriteSongsCubit()..getFavoriteSongs(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
               'FAVORITE SONGS',
             ),
-            
             const SizedBox(
               height: 20,
             ),
-            BlocBuilder<FavoriteSongsCubit,FavoriteSongsState>(
-              builder: (context,state) {
-                if(state is FavoriteSongsLoading) {
+            BlocBuilder<FavoriteSongsCubit, FavoriteSongsState>(
+              builder: (context, state) {
+                if (state is FavoriteSongsLoading) {
                   return const CircularProgressIndicator();
                 }
-                if(state is FavoriteSongsLoaded) {
+                if (state is FavoriteSongsLoaded) {
                   return ListView.separated(
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      return GestureDetector(
-                        onTap: (){
-                          Navigator.push(
-                            context, 
-                            MaterialPageRoute(
-                              builder: (BuildContext context) => SongPlayerPage(songEntity: state.favoriteSongs[index])
-                            )
-                          );
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Container(
-                                  height: 70,
-                                  width: 70,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    image: DecorationImage(
-                                      image: NetworkImage(
-                                        '${AppURLs.coverFirestorage}${state.favoriteSongs[index].artist} - ${state.favoriteSongs[index].title}.jpg?${AppURLs.mediaAlt}'
-                                      )
-                                    )
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        SongPlayerPage(
+                                            songEntity:
+                                                state.favoriteSongs[index])));
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    height: 70,
+                                    width: 70,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                '${AppURLs.coverFirestorage}${state.favoriteSongs[index].artist} - ${state.favoriteSongs[index].title}.jpg?${AppURLs.mediaAlt}'))),
                                   ),
-                                ),
-                      
-                                const SizedBox(width: 10, ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
                                   Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
                                       Text(
                                         state.favoriteSongs[index].title,
                                         style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16
-                                        ),
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 16),
                                       ),
-                                      const SizedBox(height: 5, ),
-                                        Text(
-                                          state.favoriteSongs[index].artist,
-                                          style: const TextStyle(
+                                      const SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        state.favoriteSongs[index].artist,
+                                        style: const TextStyle(
                                             fontWeight: FontWeight.w400,
-                                            fontSize: 11
-                                          ),
-                                        ),
+                                            fontSize: 11),
+                                      ),
                                     ],
                                   )
-                      
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  state.favoriteSongs[index].duration.toString().replaceAll('.', ':')
-                                ),
-                                const SizedBox(width: 20, ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(state.favoriteSongs[index].duration
+                                      .toString()
+                                      .replaceAll('.', ':')),
+                                  const SizedBox(
+                                    width: 20,
+                                  ),
                                   FavoriteButton(
                                     songEntity: state.favoriteSongs[index],
                                     key: UniqueKey(),
-                                    function: (){
-                                      context.read<FavoriteSongsCubit>().removeSong(index);
+                                    function: () {
+                                      context
+                                          .read<FavoriteSongsCubit>()
+                                          .removeSong(index);
                                     },
-                                )
-                              ],
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                    separatorBuilder: (context, index) => const SizedBox(height: 20,),
-                    itemCount: state.favoriteSongs.length
-                 );
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) => const SizedBox(
+                            height: 20,
+                          ),
+                      itemCount: state.favoriteSongs.length);
                 }
-                if(state is FavoriteSongsFailure) {
-                  return const Text(
-                    'Please try again.'
-                  );
+                if (state is FavoriteSongsFailure) {
+                  return const Text('Please try again.');
                 }
                 return Container();
-              } ,
+              },
             )
           ],
         ),
